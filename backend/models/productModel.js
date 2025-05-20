2fbe4c362c90b33af04f3a317dd66da12b7f2809
Shaqayeq-taheri <shaqayeq.taheri@gmail.com>
2025-05-20 15:09:05 +0200

    user schema is done

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,57 +1,58 @@
 import mongoose from "mongoose";
-import reviewSchema from './reviewModel.js'
+import reviewSchema from "./reviewModel.js";
 
-const productSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "User",
+const productSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User",
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        brand: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        reviews: [reviewSchema], //connecting to review schema, the reason that is an array because one product can have several reviews
+        rating: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        numReviews: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        price: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        countInstock: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
     },
-    name: {
-        type: String,
-        required: true,
-    },
-    image: {
-        type: String,
-        required: true,
-    },
-    brand: {
-        type: String,
-        required: true,
-    },
-    category: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    reviews:  [reviewSchema],//connecting to review schema, the reason that is an array because one product can have several reviews 
-    rating: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
-    numReviews: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
-    price: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
-    countInstock: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
-},{timestamps:true});
+    { timestamps: true }
+);
 
+const Product = mongoose.model("Product", productSchema);
 
-
-const Product = mongoose.model("Product", productSchema)
-
-export default Product
+export default Product;
