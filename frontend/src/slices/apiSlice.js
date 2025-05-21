09782b13e259c056b6b9cda50b6722d073a6fae6
Shaqayeq-taheri <shaqayeq.taheri@gmail.com>
2025-05-21 15:30:22 +0200

    root api slice is added

---
+++ frontend/src/slices/apiSlice.js
@@ -0,0 +1,12 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../constants";
+
+// fetchBaseQuery a function that we make query to backend api
+
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+
+export const apiSlice = createApi({
+    baseQuery,
+    tagTypes: ["Product", "User", "Order"], // to define the data we are fetching from api
+    endpoints: (builder) => ({}),
+});
