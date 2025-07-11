4e945899d8c19df3c28e847e241f46948096c9aa
Shaqayeq-taheri <shaqayeq.taheri@gmail.com>
2025-07-11 11:38:39 +0200

    auth middleware codes optimization

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,39 +1,42 @@
+// authMiddleware.js
 import jwt from "jsonwebtoken";
 import { StatusCodes } from "http-status-codes";
-import User from '../models/userModel.js'; 
+import User from "../models/userModel.js";
+import asyncHandler from "express-async-handler";
 
 
-//auth middleware for protected routes
- const authenticate = async (req, res, next) => {
-     const token = req.cookies.jwt;
+const authenticate = asyncHandler(async (req, res, next) => {
+    const token =
+        req.cookies.jwt || req.header("Authorization")?.replace("Bearer ", "");
 
-     if (!token) {
-         return res
-             .status(StatusCodes.UNAUTHORIZED)
-             .json({ message: "Unauthorized - No token provided" });
-     }
+    if (!token) {
+        res.status(StatusCodes.UNAUTHORIZED);
+        throw new Error("Not authorized, no token");
+    }
 
-     try {
-         const decoded = jwt.verify(token, process.env.JWT_SECRET); // verify token
-         req.user = await User.findById(decoded.userId).select("-password"); // attach user
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.userId).select("-password");
 
-         next();
-     } catch (error) {
-         console.error("Token verification failed:", error);
-         return res
-             .status(StatusCodes.UNAUTHORIZED)
-             .json({ message: "Unauthorized - Invalid token" });
-     }
- };
+        if (!req.user) {
+            res.status(StatusCodes.UNAUTHORIZED);
+            throw new Error("User not found");
+        }
 
-//Admin middleware
-const admin = (req, res, next) => {
-    if (req.user && req.user.isAdmin) {
         next();
-    } else {
-        res.status(401);
+    } catch (error) {
+        console.error("Authentication error:", error);
+        res.status(StatusCodes.UNAUTHORIZED);
+        throw new Error("Not authorized, token failed");
+    }
+});
+
+const admin = asyncHandler(async (req, res, next) => {
+    if (!req.user?.isAdmin) {
+        res.status(StatusCodes.FORBIDDEN);
         throw new Error("Not authorized as admin");
     }
-};
+    next();
+});
 
-export { admin, authenticate };
+export { authenticate, admin };
